b4d5d8ad3dad674d3d58f953c13dbb399126d5b2
ytvnr <yann.tavernier@graviteesource.com>
2024-06-24 16:37:06 +0200

    feat: init implementation of content provider inline https://gravitee.atlassian.net/browse/APIM-5374

---
+++ src/main/java/io/gravitee/resource/content_provider/inline/InlineContent.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.resource.content_provider.inline;
+
+import io.gravitee.resource.content_provider.api.Content;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public class InlineContent implements Content<String> {
+
+    private final String content;
+    private final Map<String, Object> attributes;
+
+    public InlineContent(String content, Map<String, Object> attributes) {
+        this.content = content;
+        if (attributes == null || attributes.isEmpty()) {
+            this.attributes = Map.of();
+        } else {
+            this.attributes = attributes;
+        }
+    }
+}
